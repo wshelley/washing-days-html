d7ddd4c4497aec829a02796acc4f214724667b1e
Wayne Shelley <wael@bgs.ac.uk>
2022-10-26 19:26:30 +0100

    tweak sun setting

--- chart.js
+++ chart.js
@@ -62,7 +62,7 @@ function reloadWashingDays() {
         forecast.day[index] = day;
         var startTime;
         forecast.rainlarge[index] = forecast.precipitation[index] * 20;
-        
+
         var sunscore = forecast.direct_radiation[index];
         var windscore = (3 *  forecast.windspeed_10m[index]);
         var humidityscore = (100 - forecast.relativehumidity_2m[index])/100;
@@ -71,7 +71,7 @@ function reloadWashingDays() {
         if (forecast.temperature_2m[index] > 0 &&
         forecast.relativehumidity_2m[index] < 80 &&
         forecast.precipitation[index] == 0 &&
-        forecast.direct_radiation[index] > 20 &&
+        forecast.direct_radiation[index] > 0 &&
         forecast.windgusts_10m[index] < 60 &&
         (forecast.windspeed_10m[index] > 10 || forecast.windgusts_10m[index] > 15))
         {
